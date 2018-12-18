b7e8610d9a0f5e2bc646700f6059a8c254bcf067
Surya <surya@keyba.se>
2018-12-18 16:21:15 -0500

    Restart on linux upgrade for systemd systems

--- packaging/linux/post_install.sh
+++ packaging/linux/post_install.sh
@@ -4,6 +4,7 @@
 # that means managing the /keybase redirector mountpoint. The deb-
 # and rpm-specific post-install scripts call into this after doing their
 # distro-specific work, which is mainly setting up package repos for updates.
+# The script also attempts to restart services if the mountpoint is not being used.
 
 set -u
 
@@ -30,6 +31,68 @@ make_mountpoint() {
       chmod 755 "$rootmount"
     fi
   fi
+}
+
+# We use machinectl here because systemctl commands do not work in either a su shell or with systemd-run --uid.
+exec_as() {
+    user=$1
+    shift
+    machinectl -q shell "$user"@ "$@"
+}
+
+# Exits with 0 iff the given user is running the service with systemd
+systemd_unit_active_for() {
+    user=$1
+    service=$2
+
+    # exit with 1 if command fails (for example, if not using systemd)
+    active_status="$(exec_as "$user" /usr/bin/systemctl --user is-active "$service")" || return "$?"
+
+    # exit code lost by machinectl, so grep stdout
+    # use -w so "active" needle doesn't match "inactive"
+    echo "$active_status" | grep -wq "active"
+}
+
+systemctl_restart_as() {
+    user=$1
+    service=$2
+    echo "Restarting $service for $user..."
+    exec_as "$user" /usr/bin/systemctl --user restart "$service"
+}
+
+systemd_safe_restart_services() {
+    pids="$(pidof /usr/bin/keybase)"
+    while read -r pid; do
+        user="$(ps -o user= -p "$pid")"
+
+        if systemd_unit_active_for "$user" "kbfs.service"; then
+            # KBFS running, need to check mounts before proceeding
+
+            # Don't use `mount(8)` to find kbfs mounts in case there are non-kbfs fuse systems
+            mount="$(exec_as "$user" /usr/bin/keybase config get -b mountdir | tr -d '\r')"
+            uses="$(exec_as "$user" /usr/bin/lsof "$mount")"
+            if [ -n "$uses" ]; then
+                using_programs="$(echo "$uses" | tail -n +2 | awk '{print $1}' | tr '\n' ', ')"
+                echo "KBFS mount $mount for user $user currently in use by ($using_programs)."
+                echo "$uses"
+                echo "Aborting Keybase autorestart for $user."
+                echo "Please restart manually by stopping the processes accessing KBFS and then running 'run_keybase' as $user."
+                continue
+            fi
+        fi
+        if systemd_unit_active_for "$user" "keybase.service"; then
+            systemctl_restart_as "$user" "keybase.service"
+        else
+            # User running keybase without systemd: give instructions on restarting.
+            echo "Keybase upgraded. Please restart Keybase manually by running 'run_keybase' as $user."
+        fi
+        if systemd_unit_active_for "$user" "keybase.gui.service"; then
+            systemctl_restart_as "$user" "keybase.gui.service"
+        fi
+        if systemd_unit_active_for "$user" "kbfs.service"; then
+            systemctl_restart_as "$user" "kbfs.service"
+        fi
+    done <<< "$pids"
 }
 
 if redirector_enabled ; then
@@ -119,3 +182,5 @@ make_mountpoint
 if command -v gtk-update-icon-cache &> /dev/null ; then
   gtk-update-icon-cache -q -t -f /usr/share/icons/hicolor
 fi
+
+systemd_safe_restart_services
